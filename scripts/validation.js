b778f3ee4d73466b379c6256197c1aecb9a2451e
speif051 <31969206+speif051@users.noreply.github.com>
2020-06-23 16:44:12 -0400

    Update validation.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -18,22 +18,6 @@ function validatePhone(txtPhone) {
 }
 
 
-// Using date restrictions on datepicker
-// Document of datepicker is here: https://api.jqueryui.com/datepicker/ 
-// The following code shows how to set specific dates to exclude, as well as Sundays (Day 0)
-// Make sure in your version that you associate Days to remove with Experts (e.g. John doesn't work Mondays)
-var unavailableDates = ["06/29/2020","07/07/2020","07/10/2020"]
-const setDateFormat = "mm/dd/yy";
-
-function disableDates(date) {
-    // Sunday is Day 0, disable all Sundays
-    if (date.getDay() == 0)
-        return [false];
-    var string = jQuery.datepicker.formatDate(setDateFormat, date);
-    return [ unavailableDates.indexOf(string) == -1 ]
-}
-
-
 // HERE, JQuery "LISTENING" starts
 $(document).ready(function(){
 
@@ -51,46 +35,5 @@ $(document).ready(function(){
             $("#phone").removeClass("error");
         }
     });
-
-    // To change the style of the calender, look in jqueryui.com, under Themes, in the ThemeRoller Gallery 
-    // You can try different themes (the names are under the calendars) / This is Excite Bike 
-    // To use a different theme you must include its css in your HTML file. 
-    // The one I included in my HTML is the Excite Bike, but you can try others
-
-    // Also, here is a good tutorial for playing with the datepicker in https://webkul.com/blog/jquery-datepicker/ 
-    // Datepicker is also documented as one of the widgets here: https://api.jqueryui.com/category/widgets/ 
-    $( "#dateInput" ).datepicker(
-        {
-            dateFormat: setDateFormat,
-            // no calendar before June 1rst 2020
-            minDate: new Date('06/01/2020'),  
-            maxDate: '+4M',
-            // used to disable some dates
-            beforeShowDay: $.datepicker.noWeekends,
-            beforeShowDay: disableDates
-        }   
-    );
-
-
-    // Look at the different events on which an action can be performed
-    // https://www.w3schools.com/jquery/jquery_events.asp
-    // Here, we put 
-    $("#debit").on("mouseenter", function(){
-        $("#debit").addClass("showInput");
-    });
-
-    $("#debit").on("mouseleave", function(){
-        $("#debit").removeClass("showInput");
-    });
-  
-    // https://jqueryui.com/tooltip/ 
-    // The class "highlight" used here is predefined in JQuery UI
-    // the message of the tooltip is encoded in the input (in the HTML file)
-    $("#debit").tooltip({
-        classes: {
-          "ui-tooltip": "highlight"
-        }
-      });
-
 
 });
