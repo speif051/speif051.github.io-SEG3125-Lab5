63c0cae526164c8db12a27464f6927e68b946468
speif051 <31969206+speif051@users.noreply.github.com>
2020-06-23 16:47:28 -0400

    Update validation.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,39 +1,18 @@
-// Function to verify that the phone number is correct.
-// Here, I validate for (12345), but you have to change that for a phone validation
-// Tutorials on Regular expressions
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions 
-// https://flaviocopes.com/javascript-regular-expressions/ 
-// Regular expressions can get complex, you can think in terms of a series of characters
-// or numbers 
-function validatePhone(txtPhone) {
-    var a = document.getElementById(phone).value;
-    
-    var filter = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
-    if (filter.test(a)){
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-
-// HERE, JQuery "LISTENING" starts
-$(document).ready(function(){
-
-    // phone validation, it calls validatePhone
-    // and also some feedback as an Alert + putting a value in the input that shows the format required
-    // the "addClass" will use the class "error" defined in style.css and add it to the phone input
-    // The "error" class in style.css defines yellow background and red foreground
-    $("#phone").on("change", function(){
-        if (!validatePhone("phone")){
-            alert("Wrong format for phone");
-            $("#phone").val("(xxxx)");
-            $("#phone").addClass("error");
+// Disable form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Get the forms we want to add validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
         }
-        else {
-            $("#phone").removeClass("error");
-        }
+        form.classList.add('was-validated');
+      }, false);
     });
-
-});
+  }, false);
+})();
